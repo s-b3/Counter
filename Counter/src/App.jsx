9f3d4312902cdfc8b2b7de73e39f0e255e49ffe2
s-b3 <sumedhab24@gmail.com>
2024-02-12 17:59:12 +0530

    first commit
    first commit

---
+++ Counter/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  let [counter, setCounter] = useState(0)
+  const addValue=()=>{
+    
+    setCounter(counter+1)
+  }
+  const removeValue=()=>{
+    if(counter != 0)
+    setCounter(counter-1)
+  }
+  
+  return (
+    <>
+      <h1>Simple Counter Using React hooks</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add Value {counter}</button>
+      <br/><br/>
+      <button onClick={removeValue}>Decrease Value {counter}</button>
+    </>
+  )
+}
+
+export default App
